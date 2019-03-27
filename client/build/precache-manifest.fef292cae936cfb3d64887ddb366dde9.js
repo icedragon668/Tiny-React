847742476cce4f973cfea8d5e1df39c7a6be9eb6
Kori <icedragon668@gmail.com>
2019-03-27 10:05:27 -0400

    build and heroku deploy

---
+++ client/build/precache-manifest.fef292cae936cfb3d64887ddb366dde9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d891e00dcce0b04c823f0032401e5fd7",
+    "url": "/static/media/kudos (2).d891e00d.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "62d2206a74815c561d1c",
+    "url": "/static/js/main.987996a2.chunk.js"
+  },
+  {
+    "revision": "0de527b21f0aed092cde",
+    "url": "/static/js/2.f087b71a.chunk.js"
+  },
+  {
+    "revision": "62d2206a74815c561d1c",
+    "url": "/static/css/main.22f433ef.chunk.css"
+  },
+  {
+    "revision": "38b9cd3b8c7b8652c3ec6125b0b663eb",
+    "url": "/index.html"
+  }
+];
